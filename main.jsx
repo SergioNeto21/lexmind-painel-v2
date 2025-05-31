861a523d7650536ddb0bc66e40f9f9d93462d72c
SergioNeto21 <sergiosnneto@gmail.com>
2025-05-30 21:27:37 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+const App = () => <h1>Painel LexMind v2</h1>
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
