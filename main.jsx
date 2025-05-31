1250fe3f3847d183bb42c00bad5e75147e5b7527
SergioNeto21 <sergiosnneto@gmail.com>
2025-05-30 21:47:46 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-const App = () => {
-  return <h1>LexMind Painel V2 Online</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
